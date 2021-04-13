1fce68bc4eabb988e2516071c70fdd7e8470060c
Sebastian F <sebpfave2@gmail.com>
2021-04-12 20:16:25 -0400

    Create os licenses info module

---
+++ utils/oslicenses.js
@@ -0,0 +1,50 @@
+const osLicenses = {
+  "Apache License 2.0": {
+    code: "Apache 2.0",
+    link: "https://opensource.org/licenses/Apache-2.0",
+  },
+  "GNU General Public License v3.0": {
+    code: "GNU GPLv3",
+    link: "https://www.gnu.org/licenses/gpl-3.0",
+  },
+  "MIT License": { code: "MIT", link: "https://opensource.org/licenses/MIT" },
+  "BSD 3-Clause License": {
+    code: "BSD 3-Clause",
+    link: "https://opensource.org/licenses/BSD-3-Clause",
+  },
+  "BSD 2-Clause License": {
+    code: "BSD 2-Clause",
+    link: "https://opensource.org/licenses/BSD-2-Clause",
+  },
+  "Boost Software License 1.0": {
+    code: "Boost 1.0",
+    link: "https://www.boost.org/LICENSE_1_0.txt",
+  },
+  "Creative Commons Zero v1.0 Universal": {
+    code: "CC0 1.0",
+    link: "http://creativecommons.org/publicdomain/zero/1.0/",
+  },
+  "Eclipse Public License 2.0": {
+    code: "Eclipse 2.0",
+    link: "https://opensource.org/licenses/EPL-2.0",
+  },
+  "GNU Affero General Public License v3.0": {
+    code: "GNU AGPLv3",
+    link: "https://www.gnu.org/licenses/agpl-3.0",
+  },
+  "GNU General Public License v2.0": {
+    code: "GNU GPLv2",
+    link: "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html",
+  },
+  "GNU Lesser General Public License v2.1": {
+    code: "GNU LGPL v3",
+    link: "https://www.gnu.org/licenses/lgpl-3.0",
+  },
+  "Mozilla Public License 2.0": {
+    code: "MPL 2.0",
+    link: "https://opensource.org/licenses/MPL-2.0",
+  },
+  "The Unlicense": { code: "Unlicense", link: "http://unlicense.org/" },
+};
+
+module.exports = osLicenses;
